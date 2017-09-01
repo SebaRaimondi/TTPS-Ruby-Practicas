a4908486c5cc55b2d7ef9e9c3e01dd1cc1a21e7d
Sebastian <sebastianraimondi@gmail.com>
2017-09-01 19:34:10 -0300

    Practica 1 Ejercicio 8

---
+++ Practica-01/E8.rb
@@ -0,0 +1,10 @@
+#	8. ¿Qué hace el siguiente código?
+
+[:upcase, :downcase, :capitalize, :swapcase].map do | meth|
+	"TTPS Ruby".send(meth)
+end
+
+# 	Le envia un arreglo de 4 simbolos que coinciden con 4 mensajes que 
+# 	entienden los string. Luego itera por el arreglo, enviandole cada
+#	uno de los mensajes al string "TTPS Ruby", guardando el resultado
+#	en un nuevo arreglo.
