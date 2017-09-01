6efcfe58dec2453c25c6488741f7964c46b91a72
Sebastian <sebastianraimondi@gmail.com>
2017-09-01 19:14:00 -0300

    Practica 1, Ejercicion 6

---
+++ Practica-01/E6.rb
@@ -0,0 +1,12 @@
+#	6. Modificá la función anterior para que sólo considere como aparición del segundo string
+#	cuando se trate de palabras completas. Por ejemplo:
+
+#	contar_palabras("La casa de la esquina tiene la puerta roja y la ventana blan
+#	ca.", "la")
+#	# => 4
+
+def contar(str1, str2)
+	str1.downcase.scan(str2.downcase).split.count { |x| x == "la" }
+end
+
+contar("La casa de la esquina tiene la puerta roja y la ventana blanca.", "la")
