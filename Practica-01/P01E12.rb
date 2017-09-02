947c1154c7be5fcd13b328f17f2cc840980e9674
Sebastian <sebastianraimondi@gmail.com>
2017-09-01 21:14:15 -0300

    Practica 1, Ejercicio 12. Mismo problema que E11

---
+++ Practica-01/P01E12.rb
@@ -0,0 +1,15 @@
+#	12. Escribí una función más genérica, parecida a la del ejercicio anterior, que reciba como
+#	parámetro un string y un número n, y que realice una rotación de n lugares de las letras
+#	del string y retorne el resultado. Por ejemplo:
+
+#	rot("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!", 13)
+#	# => "¡Ovrairavqbf n yn phefnqn 2015 qr GGCF Bcpvóa Ehol!"
+
+def rot(str, i = 1)
+	i.times { str.gsub!(/[A-Za-z]/) { |c| c.succ } }
+	return str
+end
+
+rot("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!", 13)
+
+#	Preguntar, mismo problema que E11
