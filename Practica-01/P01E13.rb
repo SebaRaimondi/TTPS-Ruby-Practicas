0c94c88b9011d014776d46334a6267787b0a5685
Sebastian <sebastianraimondi@gmail.com>
2017-09-01 21:19:05 -0300

    Practica 1, Ejercicio 13

---
+++ Practica-01/P01E13.rb
@@ -0,0 +1,11 @@
+#	13. Escribí un script en Ruby que le pida al usuario su nombre y lo utilice para saludarlo
+#	imprimiendo en pantalla ¡Hola, <nombre>!. Por ejemplo:
+
+#	$ ruby script.rb
+#	Por favor, ingresá tu nombre:
+#	Matz
+#	¡Hola, Matz!
+
+puts "Por favor, ingresa tu nombre:"
+name = gets.chomp
+puts "Hola, #{name}!"
