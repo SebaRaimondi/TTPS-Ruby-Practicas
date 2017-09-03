9b58d91c92cf32852729c59518086dbfe199c069
Sebastian <sebastianraimondi@gmail.com>
2017-09-03 15:48:47 -0300

    Saque un .times, agregue recursion.

--- Practica-01/P01E11.rb
+++ Practica-01/P01E11.rb
@@ -4,12 +4,28 @@
 #	rot13("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!")
 #	# => "¡Ovrairavqbf n yn phefnqn 2015 qr GGCF Bcpvóa Ehol!"
 
-def rot13(str)
-	13.times { str.gsub!(/[A-Za-z]/) { |c| c.succ } }
-	return str
-end
+#	def rot13(str)
+#		13.times { str.gsub!(/[A-Za-z]/) { |c| c.succ } }
+#		return str
+#	end
 
-print rot13("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!")
+#	print rot13("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!")
 
 
 #	=> "¡Ovraaiiraavqbbff n yn phheeffnqn 2015 qr GGGGCCFF BBccpvóaa EEhholl!"
+
+#	Version 2
+
+def nextChar(n, char)
+	if n > 0
+		return nextChar(n-1, char.succ)
+	else
+		return char
+	end
+end
+
+def rot13(str)
+	str.gsub(/[A-Za-z]/) { |c| nextChar(13, c) }
+end
+
+p rot13("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!")
