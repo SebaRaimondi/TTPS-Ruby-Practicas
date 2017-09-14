4441e7d920be9cfd6e811c54e39f492b7ace0844
Sebastian <sebastianraimondi@gmail.com>
2017-09-14 19:12:01 -0300

    Ejercicio 3 Practica 2

--- Practica-02/P02E03.rb
+++ Practica-02/P02E03.rb
@@ -1,7 +1,10 @@
 # 3. Suponé que se te da el método que implementaste en el ejercicio anterior para que lo uses a fin de ordenar un arreglo de números que te son provistos en forma de arreglo.
 # ¿Cómo podrías invocar el método? Por ejemplo, teniendo la siguiente variable con los números a ordenar:
 
-
+def ordenar(*params)
+	params.flatten.sort
+end
 
 entrada = [10, 9, 1, 2, 3, 5, 7, 8]
+p ordenar(entrada)
 # Dada `entrada', invocar a #ordenar utilizando sus valores para ordenarlos
