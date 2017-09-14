6945b6bf25cde8a30e1dbc027990782417ea2c20
Sebastian <sebastianraimondi@gmail.com>
2017-09-14 19:45:23 -0300

    Ejercicio 5

---
+++ Practica-02/P02E05.rb
@@ -0,0 +1,11 @@
+# 5. Implementá el método cuanto_falta? que opcionalmente reciba como parámetro un objeto Time y que calcule la cantidad de minutos que faltan para ese momento. Si el parámetro de fecha no es provisto, asumí que la consulta es para la medianoche de hoy.
+# Por ejemplo:
+
+def cuanto_falta?(time = Time.new)
+	Time.new(time.year, time.month, time.day, 23, 59, 59) - time
+end
+
+p cuanto_falta? Time.new(2017, 12, 31, 23, 59, 59)
+# Debe retornar la cantidad de minutos que faltan para las 23:59:59 del 31/12/2017
+p cuanto_falta?
+# Debe retornar la cantidad de minutos que faltan para la medianoche de hoy
